ff5216b28560567fdedeaa8735e55c5f46aa059a
Shane <sreilly@udel.edu>
2024-11-20 23:56:01 -0500

    Fixed issue with incorrect imports

--- src/main/java/com/attacktimer/AttackTimerMetronomePlugin.java
+++ src/main/java/com/attacktimer/AttackTimerMetronomePlugin.java
@@ -56,13 +56,13 @@ import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.game.ItemEquipmentStats;
 import net.runelite.client.game.ItemManager;
+import net.runelite.client.game.ItemStats;
 import net.runelite.client.game.NPCManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.ui.overlay.OverlayManager;
-import net.runelite.http.api.item.ItemEquipmentStats;
-import net.runelite.http.api.item.ItemStats;
 
 @Slf4j
 @PluginDescriptor(
